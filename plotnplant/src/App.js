9727063a512c07e606c8030300620118d16bbe60
sad00013 <sad00013@mix.wvu.edu>
2024-03-01 11:26:27 -0500

    Calendar
    Implemented calendar component.

--- plotnplant/src/App.js
+++ plotnplant/src/App.js
@@ -1,5 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
+ 
+import React from 'react'
+import { Calendar } from 'rsuite'; 
 
 function App() {
   return (
@@ -7,6 +10,7 @@ function App() {
       <title>Plot N' Plant</title>
       <h1>Plot N' Plant</h1>
       <img src="https://cdn1.iconfinder.com/data/icons/icons-for-a-site-1/64/advantage_eco_friendly-128.png" alt = "Plant"></img>
+      <Calendar compact bordered />
     </div>
   );
 }
