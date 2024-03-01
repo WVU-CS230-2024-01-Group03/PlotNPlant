aa74927b31e40675d782645542b0d871ab8c4489
stevenporip <sp00032@mix.wvu.edu>
2024-03-01 13:27:48 -0500

    Second version of login and register page

--- plotnplant/src/App.js
+++ plotnplant/src/App.js
@@ -1,12 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
 import LoginPage from './Components/LoginPage';
+import RegisterPage from './Components/RegisterPage';
+import * as React from "react";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <title>Plot N' Plant</title>
-      <LoginPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- plotnplant/src/Components/LoginPage.jsx
+++ plotnplant/src/Components/LoginPage.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import './LoginPage.css';
+import { FaUser, FaLock} from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+
 
 const LoginPage = () => {
   return(
-      <div className='wrapper'>
-        <h1>Plot N' Plant<img src="https://cdn1.iconfinder.com/data/icons/icons-for-a-site-1/64/advantage_eco_friendly-128.png" alt = "Plant" width="80px"></img></h1>
-        
+      <div class='wrapper'>
+        <h1>Plot N' Plant</h1>
+        <img src="https://cdn1.iconfinder.com/data/icons/icons-for-a-site-1/64/advantage_eco_friendly-128.png" alt = "Plant" width="80px"></img>
 
         <form action="">
           <div className='input-box'>
             <input type="text" placeholder='Username' required />
+            <FaUser className='icon'/>
           </div>
           <div className='input-box'>
             <input type="password" placeholder='Password' required />
+            <FaLock className='icon'/>
           </div>
 
           <button type="submit">Login</button>
+          
 
-          <div className="register-account">
-            <p>Don't have an account? <a href="#">Register</a></p>
-          </div>
+            <div className="register-account">
+              <p>Don't have an account? <Link to="/register">Register</Link></p>
+            </div>
+
         </form>
       </div>
 
