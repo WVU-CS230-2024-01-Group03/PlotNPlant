5bebb2fcd3681d33fc10f850d7ffc3bb9c6478df
Zcman03 <zct00003@mix.wvu.edu>
2024-03-03 14:13:33 -0500

    added css styling

--- plotnplant/src/Components/RegisterPage.jsx
+++ plotnplant/src/Components/RegisterPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './RegisterPage.css';
 import { Link } from 'react-router-dom';
 
 const RegisterPage = () =>{
   return(
     <div class='wrapper'>
+    <h1>Plot N' Plant</h1>
     <img src="https://cdn1.iconfinder.com/data/icons/icons-for-a-site-1/64/advantage_eco_friendly-128.png" alt = "Plant" width="80px"></img>
     <form action="">
     <div className='input-box'>
