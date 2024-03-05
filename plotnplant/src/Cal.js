051459891cb55265118a9c45cc29fe2826e1c891
sad00013 <sad00013@mix.wvu.edu>
2024-03-05 15:29:29 -0500

    Did some browser routing

---
+++ plotnplant/src/Cal.js
@@ -0,0 +1,10 @@
+import { Calendar } from 'rsuite';
+function Cal() {
+    return (
+      <div className="Cal">
+        <Calendar compact bordered />
+      </div>
+    );
+  }
+
+  export default Cal;
