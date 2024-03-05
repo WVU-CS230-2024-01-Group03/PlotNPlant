5878d9cc6e325123fe21348091d6577fcc8776c5
sad00013 <sad00013@mix.wvu.edu>
2024-03-05 15:30:01 -0500

    Did browser router for cal

--- plotnplant/src/App.js
+++ plotnplant/src/App.js
@@ -1,16 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
- 
-import React from 'react'
-import { Calendar } from 'rsuite'; 
-
+import Cal from './Cal';
+import * as React from "react";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+ // import { Calendar } from 'rsuite'; 
 function App() {
   return (
     <div className="App">
-      <title>Plot N' Plant</title>
-      <h1>Plot N' Plant</h1>
-      <img src="https://cdn1.iconfinder.com/data/icons/icons-for-a-site-1/64/advantage_eco_friendly-128.png" alt = "Plant"></img>
-      <Calendar compact bordered />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Cal />} />
+      </Routes>
+    </BrowserRouter>
+
+      {/* <img src="https://cdn1.iconfinder.com/data/icons/icons-for-a-site-1/64/advantage_eco_friendly-128.png" alt = "Plant"></img>
+      <Calendar compact bordered /> */}
     </div>
   );
 }
--- plotnplant/src/Cal.js
+++ plotnplant/src/Cal.js
@@ -2,6 +2,9 @@ import { Calendar } from 'rsuite';
 function Cal() {
     return (
       <div className="Cal">
+        <title>Plot N' Plant</title>
+        <h1>Plot N' Plant</h1>
+        <img src="https://cdn1.iconfinder.com/data/icons/icons-for-a-site-1/64/advantage_eco_friendly-128.png" alt = "Plant"></img>
         <Calendar compact bordered />
       </div>
     );
