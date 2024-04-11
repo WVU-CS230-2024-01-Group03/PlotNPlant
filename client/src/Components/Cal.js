5d02fbd5b7affd44341d8af0340b1bce402dbf02
sad00013 <sad00013@mix.wvu.edu>
2024-04-11 13:44:59 -0400

    Initial Commit
    Staging changes to start working on custom event creation

--- client/src/Components/Cal.js
+++ client/src/Components/Cal.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import './Cal.css';
 import {Whisper, Popover, Badge } from 'rsuite';
 
+//Partial implementation of the custom events
+
 //WIP
 // const todo = new Map();
 
@@ -13,15 +15,26 @@ import {Whisper, Popover, Badge } from 'rsuite';
 //   todo.set(date, text);
 // }
 
+//WIP
+// function getTodoList(date) {
+//   const day = date.getDate();
+//   const day = document.getElementById("day").value;
+//   if(todo.get(day)){
+//     return todo.get(day);
+//   }
+//   return [];
+// }
 
+
+//This is the method that has the hard coded events to the table
 function getTodoList(date) {
   const day = date.getDate();
 
   switch (day) {
     case 12:
       return [
-        { time: '10:30 am', title: 'Green Beans' },
-        { time: '12:00 pm', title: 'Peppers' }
+        { time: '10:30 am', title: day },
+        { time: '12:00 pm', title: day }
       ];
     case 15:
       return [
@@ -31,15 +44,6 @@ function getTodoList(date) {
       return [];
   }
 }
-
-//WIP
-// function getTodoList() {
-//   const day = document.getElementById("day").value;
-//   if(todo.get(day)){
-//     return todo.get(day);
-//   }
-//   return [];
-// }
 
 function renderCell(date) {
   const list = getTodoList(date);
@@ -89,13 +93,13 @@ function Cal() {
         <h1>Plot N' Plant</h1>
         <img src="https://cdn1.iconfinder.com/data/icons/icons-for-a-site-1/64/advantage_eco_friendly-128.png" alt = "Plant"></img>
         <p><Link to="/">Login</Link></p>
-        <form>
+        {/* <form>
           <label for="newEvent">New Event:</label>
           <input type="date" id="day" name="date"></input><br></br>
           <label for="notes">Information:</label>
           <input type="text" id="info" name="info"></input>
           <button onclick="newTodo()">Click me</button>
-        </form>
+        </form> */}
         {/* <Calendar compact bordered  /> */}
         {<Calendar compact bordered  renderCell={renderCell}/>}
       </div>
