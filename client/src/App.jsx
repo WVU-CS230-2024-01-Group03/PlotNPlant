2f0432f29c3733345db56f9c0c8f695f423f51d9
Zcman03 <zct00003@mix.wvu.edu>
2024-04-28 20:32:45 -0400

    live site with temp page to add NOAA data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import UserSettings from "./Components/UserSettings";
 import Map from "./Components/Map";
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DataParser from "./Components/tempdatatransfer";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/usersettings" element={<UserSettings />} />
           <Route path="/resetpassword" element={<ResetPassword />} />
           <Route path="/map" element={<Map />} />
+          <Route path="/data" element={<DataParser/>} />
         </Routes>
       </BrowserRouter>
     </div>
